967521842f8d61b2d7f700ff0b3c6c57db53f578
cheasath <asathasath435@gmail.com>
2025-01-10 09:02:22 +0530

    commit all files

---
+++ javascript/print.js
@@ -0,0 +1,22 @@
+a="SECE"
+console.log(a)
+b=true
+console.log(b)
+console.log(typeof(b))
+array=['a',1]
+console.log(array)
+obj={
+    firstname: "michael",
+    lastname: "Smith",
+}
+console.log(obj.firstname);
+obj2 = new Object();
+console.log(obj2);
+obj2.count = 70
+obj2.abs = 1
+totalPresentCount = obj2.count - obj2.abs
+console.log(totalPresentCount);
+console.log(obj2);
+set = new Set("hello",1,2,"world");
+console.log(typeof(set));
+console.log(set);
