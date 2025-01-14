5be827430b47cfaf31f504de2438b3453265cafc
cheasath <asathasath435@gmail.com>
2025-01-14 17:51:38 +0530

    committed

--- javascript/asyncAwait.js
+++ javascript/asyncAwait.js
@@ -1,11 +1,46 @@
-async function asAw()
-{
-    setTimeout(()=>{
-        return ("inside timeout")
-    },5000)
-return "this is async/await example"
+/*async function asAw(){
+    setTimeout(() => {
+        return("Inside setTimeout");
+    }, 5000);
+    //console.log("This is async and await function")
 }
-asAw().then(function(output)
-{
-console.log(output)
-})
+
+ //console.log(asAw())
+
+ asAw().then((data)=>{
+     console.log(data)
+ })*/
+
+     likeCode=async()=>{
+    
+        return new Promise((likingPost)=>{
+          setTimeout(()=>{
+          likingPost("Liked the post created")
+        });
+      })
+      }
+      
+      commentCode=async()=>{
+          
+          return new Promise((commentingPost)=>{
+            setTimeout(()=>{
+            commentingPost("comment the post created")
+          });
+        })
+        }
+      
+       
+      
+       async function createPost(){
+          var post=new Promise((cPost)=>{
+              setTimeout(()=>{
+               cPost("Post Created Successfully")
+              },5000)
+          });
+          var[post,like,comment]=await Promise.all([
+              post,
+              likCode(),
+              commentCode()])
+      createPost()
+      
+          }
