88d8531ac6f5a47c33b94409d195729b7e7ba6cd
cheasath <asathasath435@gmail.com>
2025-01-21 09:09:39 +0530

    commit

---
+++ React_project/src/Components/ClassCompEG.jsx
@@ -0,0 +1,16 @@
+import React from 'react'; // Ensure React is imported
+
+class ClassCompEG extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>
+                    This is a class component
+                    
+                </h1>
+            </div>
+        );
+    }
+}
+
+export default ClassCompEG;
