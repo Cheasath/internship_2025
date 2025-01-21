5293354a3b51e36b3ff9638c069ebfef91f0f0aa
cheasath <asathasath435@gmail.com>
2025-01-21 14:45:13 +0530

    commit

--- React_project/src/App.jsx
+++ React_project/src/App.jsx
@@ -4,15 +4,25 @@ import viteLogo from '/vite.svg';
 import ClassCompEG from './Components/ClassCompEG.jsx'; 
 import Contact from './Components/Contact.jsx';
 import Gallery from './Components/Gallery.jsx';
+import Home from './Components/Home.jsx';
+import About from './About.jsx';
 import './App.css';
-
+import Navbar from './Components/Navbar.jsx';
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 function App() {
   const [count, setCount] = useState(0)
   return (
     <div>
-       <ClassCompEG />
-       <Contact/>
-       <Gallery image="SECE Logo" />
+       <BrowserRouter>
+      <Navbar/>
+        <Routes>
+          <Route path='/home' element={<Home/>}></Route>
+          <Route path='/about' element={<About/>}></Route>
+          <Route path='/gallery' element={< Gallery image="React LOGO" page="picture"/>}></Route>
+          <Route path='/Contact' element={<Contact/>}></Route>
+        </Routes>         
+    </BrowserRouter>
+       
     </div>
    
   );
--- React_project/src/Components/Gallery.jsx
+++ React_project/src/Components/Gallery.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
-export default class Gallery extends React.Component {
-    render() {
-        return (
-            <>
-            </>
-        );
+const Gallery=(props1)=>{
+
+        return(
+            <div>
+                <p>Inside the gallery</p>
+                <h3>Welcome to {props1.page} page</h3>
+                <img src ="https://pbs.twimg.com/profile_images/1169109470967820291/AJzI4C-S_400x400.jpg"></img>
+                <h3>Viewing {props1.image}</h3>
+                
+                </div>
+            )
     }
-}
+    
+export default Gallery;
